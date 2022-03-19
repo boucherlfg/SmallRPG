f104ae10adc245209c3076af8f7233475c02de13
Felix Boucher <boucherlfg@gmail.com>
2022-03-18 21:53:55 -0400

    fixed issue with 0 durability

--- Assets/Scripts/ScriptableObjects/Items/Item.cs
+++ Assets/Scripts/ScriptableObjects/Items/Item.cs
@@ -1,37 +1,17 @@
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-
-[CustomEditor(typeof(Item), editorForChildClasses:true), CanEditMultipleObjects]
-public class ItemEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        var obj = target as Item;
-        DrawDefaultInspector();
-        obj.hasDurability = EditorGUILayout.Toggle("Has Durability", obj.hasDurability);
-        if (obj.hasDurability)
-        {
-            obj.durability = EditorGUILayout.IntField("Durability", obj.durability);
-        }
-    }
-}
-#endif
-
 
 [InventoryCategory]
 public abstract class Item : ScriptableObject
 {
-    [HideInInspector]
-    public bool hasDurability;
-    [HideInInspector]
-    public int durability;
+   
 
     public string visibleName;
     public Sprite sprite;
     public string description;
     public float value;
+
+    public int durability;
 
     static int position = 1;
     public void Prepare()
