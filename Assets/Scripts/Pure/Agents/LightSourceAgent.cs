4aae427cd90831d67e0ac3647ba08da51c703b0f
Felix Boucher <boucherlfg@gmail.com>
2022-03-20 18:16:56 -0400

    more imbued knives + durability bug fix

---
+++ Assets/Scripts/Pure/Agents/LightSourceAgent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Tilemaps;
+
+public class LightSourceAgent : Agent, ILightSource, IDrawable, IActivatable, IEndable, ICollision
+{
+    public Tile CurrentTile => GameHelper.CreateTile(lightSource.sprite);
+
+    public LightSource lightSource;
+
+    public float Range => lightSource.range;
+
+    public void Activate(IMovable source)
+    {
+
+        Game.Instance.Destroy(this);
+    }
+
+    public void End()
+    {
+        AudioManager.PlayAsSound("empty");
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Items/LightSource.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Felix/Items/Light Source")]
+public class LightSource : Placeable
+{
+    public float range = 5;
+    public override Agent AgentFactory => new LightSourceAgent() { lightSource = this };
+}
